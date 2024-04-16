a8227825226f0b7307c28795e4516841f6c295c6
StudentYoup <s1198594@student.windesheim.nl>
2024-04-16 14:10:30 +0200

    Test

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ package MagazijnRobot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Zorgboerderij!");
-        System.out.println("Zorgboerderij!");
-        System.out.println("Zorgboerderij!");
+        System.out.println("Main - Zorgboerderij!");
     }
 }

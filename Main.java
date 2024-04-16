c19ddc433b640fb8217cc6de41b431b404dcf961
StudentYoup <s1198594@student.windesheim.nl>
2024-04-16 14:20:01 +0200

    Start

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package MagazijnRobot;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("### Main ###");
         System.out.println("Main - Zorgboerderij!");
     }
 }
